b9d794f32cf64d3403e889b6899eee969ef18257
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2022-04-20 14:38:18 +0300

    some fixes

--- src/components/productBlock.jsx/index.jsx
+++ src/components/productBlock.jsx/index.jsx
@@ -10,7 +10,7 @@ const ProductBlock = ({tabs, product, heading}) => {
         <div className='product-block'>
             <Heading title={heading.title} link={heading.link} titleLink={heading.titleLink} className='product-block__heading' />
             <TabsWrapper className='product-block__tabs'>
-                {tabs.map(item => <Tab key={item.id} text={item.title} />)}
+                {tabs.map(item => <Tab key={item.id} text={item.title} active={item.active} />)}
             </TabsWrapper>
             <ProductSlider items={product} />
         </div>
